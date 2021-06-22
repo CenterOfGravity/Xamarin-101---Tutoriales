44fb79c4e537569c006930a04ffc70e206369d03
jhanif.kara <jhanif.kara@outlook.com>
2021-06-22 11:44:42 -0500

    Add project files.

---
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PrimeraPaginaView : ContentPage
+    {
+        public PrimeraPaginaView()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushModalAsync(new SegundaPaginaView());
+        }
+    }
+}
---
+++ App2/App2/View/SegundaPaginaView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SegundaPaginaView : ContentPage
+    {
+        public SegundaPaginaView()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button2_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+        }
+    }
+}
