3eadb521973590049eae0d14b8992291e7d0952e
jhanif.kara <jhanif.kara@outlook.com>
2021-07-30 10:48:01 -0500

    PreferencesAndSwitch

--- App2/App2/View/PrimeraPaginaView.xaml.cs
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+using Xamarin.Essentials;
 namespace App2.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,13 +10,28 @@ namespace App2.View
         public PrimeraPaginaView()
         {
             InitializeComponent();
+            EntryParaNombre.Text = Preferences.Get("NombreGuardado","");
+            EntryParaApellido.Text = Preferences.Get("ApellidoGuardado", "");
+            RememberMeSwith.IsToggled = Preferences.Get("RememberMeSwithState", false);
         }
 
-        private async void Button1_Clicked(object sender, EventArgs e)
+        private void RememberMeSwith_Toggled(object sender, ToggledEventArgs e)
         {
-            string nombre = EntryParaNombre.Text;
-            string apellido = EntryParaApellido.Text;
-            await Application.Current.MainPage.Navigation.PushAsync(new SegundaPaginaView(nombre, apellido));
+          if(RememberMeSwith.IsToggled == true)
+          {
+                Preferences.Set("NombreGuardado", EntryParaNombre.Text);
+                Preferences.Set("ApellidoGuardado", EntryParaApellido.Text);
+                Preferences.Set("RememberMeSwithState", RememberMeSwith.IsToggled);
+                RememberMeSwith.ThumbColor = Color.Blue;
+                RememberMeSwith.OnColor = Color.SkyBlue;
+            }
+          else
+          {
+                Preferences.Remove("NombreGuardado");
+                Preferences.Remove("ApellidoGuardado");
+                Preferences.Remove("RememberMeSwithState");
+                RememberMeSwith.ThumbColor = Color.Gray;
+            }
         }
     }
 }
