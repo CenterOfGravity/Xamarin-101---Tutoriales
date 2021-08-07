c763fd081b0e51ff3f147d8e4cb9a46605bc2a09
jhanif.kara <jhanif.kara@outlook.com>
2021-08-07 12:29:50 -0500

    StopWatch with Pressed and Release

--- App2/App2/Views/PrimeraPaginaView.xaml.cs
+++ App2/App2/Views/PrimeraPaginaView.xaml.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 namespace App2.Views
@@ -9,6 +9,7 @@ namespace App2.Views
     {
         bool EstaElTiempoCorriendo;
         int incremento;
+        Stopwatch cronometro = new Stopwatch();
         public PrimeraPaginaView()
         {
             InitializeComponent();
@@ -18,16 +19,30 @@ namespace App2.Views
         {
             EstaElTiempoCorriendo = true;
             incremento = 1;
+
+            cronometro.Restart();
             Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
             {
-                ContadorAutomaticoLabel.Text = (Convert.ToInt64(ContadorAutomaticoLabel.Text) + incremento).ToString();
-                return EstaElTiempoCorriendo;
+                if ((int)cronometro.ElapsedMilliseconds >= 10000)
+                {
+                    incremento = 100000;
+                }
+                else if ((int)cronometro.ElapsedMilliseconds >= 5000)
+                {
+                    incremento = 1000;
+                }
+
+                ContadorAutomaticoLabel.Text = (Convert.ToInt64(ContadorAutomaticoLabel.Text) 
+                                                                       + incremento).ToString();
+
+              return EstaElTiempoCorriendo;
             });
         }
 
         private void Button_Released(object sender, EventArgs e)
         {
             EstaElTiempoCorriendo = false;
+            cronometro.Stop();
         }
     }
 }
