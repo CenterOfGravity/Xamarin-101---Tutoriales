5f2494fdfc353c51bf762c3c04894869a70397f2
jhanif.kara <jhanif.kara@outlook.com>
2021-12-15 19:46:09 -0500

    justforfun, load and add file file to app

--- App2/App2/View/PrimeraPaginaView.xaml.cs
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Reflection;
+using System.Xml;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,14 +10,16 @@ namespace App2.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PrimeraPaginaView : ContentPage
     {
+        XmlDocument xmlinfodataloaded = new XmlDocument();
         public PrimeraPaginaView()
         {
             InitializeComponent();
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(PrimeraPaginaView)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("App2.InfomacionEnXml.xml");
+            xmlinfodataloaded.Load(stream);
+            lbl_RawXmlData.Text = xmlinfodataloaded.InnerXml;
         }
 
-        private async void Button1_Clicked(object sender, EventArgs e)
-        {
-            await Application.Current.MainPage.Navigation.PushModalAsync(new SegundaPaginaView());
-        }
+
     }
 }
