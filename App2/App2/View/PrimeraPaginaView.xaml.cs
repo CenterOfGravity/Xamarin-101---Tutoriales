1347d8dfa5276894deb13728bef5cee769e2b28b
jhanif.kara <jhanif.kara@outlook.com>
2021-12-16 11:12:04 -0500

    Leer Informacion en XML en C# y Xamarin MAUI

--- App2/App2/View/PrimeraPaginaView.xaml.cs
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Xml;
 
@@ -11,15 +12,36 @@ namespace App2.View
     public partial class PrimeraPaginaView : ContentPage
     {
         XmlDocument xmlinfodataloaded = new XmlDocument();
+        XmlNodeList CountryNodeList, ProvinciasNodeList, ProvinciaNodeList; 
         public PrimeraPaginaView()
         {
             InitializeComponent();
             var assembly = IntrospectionExtensions.GetTypeInfo(typeof(PrimeraPaginaView)).Assembly;
             Stream stream = assembly.GetManifestResourceStream("App2.InfomacionEnXml.xml");
             xmlinfodataloaded.Load(stream);
-            lbl_RawXmlData.Text = xmlinfodataloaded.InnerXml;
+            CountryNodeList = xmlinfodataloaded.GetElementsByTagName("País");
+
+            List<string> countryNameDataList = new List<string>();
+            foreach (XmlNode country in CountryNodeList)
+            {
+                countryNameDataList.Add(country.Attributes["label"].Value);
+            }
+            Picker_Country.ItemsSource = countryNameDataList;
         }
 
+        private void Picker_Country_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            XmlElement CountryElements = (XmlElement)CountryNodeList[Picker_Country.SelectedIndex];
+            ProvinciasNodeList = CountryElements.GetElementsByTagName("Provincias");
+            XmlElement ProvinciasElements = (XmlElement)ProvinciasNodeList[0];
+            ProvinciaNodeList = ProvinciasElements.GetElementsByTagName("provincia");
 
+            List<string> ProvinciaNameDataList = new List<string>();
+            foreach (XmlNode provincia in ProvinciaNodeList)
+            {
+                ProvinciaNameDataList.Add(provincia.InnerText);
+            }
+            Picker_Provincia.ItemsSource = ProvinciaNameDataList;
+        }
     }
 }
