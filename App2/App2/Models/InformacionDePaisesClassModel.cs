a817732fba06b00ea0e5c669ad5fa679fe84edd9
jhanif.kara <jhanif.kara@outlook.com>
2022-08-10 07:41:21 -0500

    first comming in json project

---
+++ App2/App2/Models/InformacionDePaisesClassModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    class InformacionDePaisesClassModel
+    {
+
+        public Dictionary<string, Array[]> PaisesInfo { get; set; }
+          
+    }
+}
--- App2/App2/View/PrimeraPaginaView.xaml.cs
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -2,7 +2,8 @@
 using System.IO;
 using System.Reflection;
 using System.Xml;
-
+using App2.Models;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,37 +12,42 @@ namespace App2.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PrimeraPaginaView : ContentPage
     {
-        XmlDocument xmlinfodataloaded = new XmlDocument();
         XmlNodeList CountryNodeList, ProvinciasNodeList, ProvinciaNodeList; 
         public PrimeraPaginaView()
         {
             InitializeComponent();
             var assembly = IntrospectionExtensions.GetTypeInfo(typeof(PrimeraPaginaView)).Assembly;
-            Stream stream = assembly.GetManifestResourceStream("App2.InfomacionEnXml.xml");
-            xmlinfodataloaded.Load(stream);
-            CountryNodeList = xmlinfodataloaded.GetElementsByTagName("País");
-
-            List<string> countryNameDataList = new List<string>();
-            foreach (XmlNode country in CountryNodeList)
+            Stream stream = assembly.GetManifestResourceStream("App2.InformacionEnJson.json");
+            InformacionDePaisesClassModel Informacion;
+            string ReadData;
+            using (var streamReader = new StreamReader(stream))
             {
-                countryNameDataList.Add(country.Attributes["label"].Value);
+                ReadData = streamReader.ReadToEnd();
             }
-            Picker_Country.ItemsSource = countryNameDataList;
+             Informacion = JsonConvert.DeserializeObject<InformacionDePaisesClassModel>(ReadData);
+
+            name.Text = Informacion.PaisesInfo.ToString();
+            //List<string> countryNameDataList = new List<string>();
+            foreach (var info in Informacion.PaisesInfo)
+            {
+             //   countryNameDataList.Add(country.Attributes["label"].Value);
+            }
+      //      Picker_Country.ItemsSource = countryNameDataList;
         }
 
         private void Picker_Country_SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            XmlElement CountryElements = (XmlElement)CountryNodeList[Picker_Country.SelectedIndex];
-            ProvinciasNodeList = CountryElements.GetElementsByTagName("Provincias");
-            XmlElement ProvinciasElements = (XmlElement)ProvinciasNodeList[0];
-            ProvinciaNodeList = ProvinciasElements.GetElementsByTagName("provincia");
+            //XmlElement CountryElements = (XmlElement)CountryNodeList[Picker_Country.SelectedIndex];
+            //ProvinciasNodeList = CountryElements.GetElementsByTagName("Provincias");
+            //XmlElement ProvinciasElements = (XmlElement)ProvinciasNodeList[0];
+            //ProvinciaNodeList = ProvinciasElements.GetElementsByTagName("provincia");
 
-            List<string> ProvinciaNameDataList = new List<string>();
-            foreach (XmlNode provincia in ProvinciaNodeList)
-            {
-                ProvinciaNameDataList.Add(provincia.InnerText);
-            }
-            Picker_Provincia.ItemsSource = ProvinciaNameDataList;
+            //List<string> ProvinciaNameDataList = new List<string>();
+            //foreach (XmlNode provincia in ProvinciaNodeList)
+            //{
+            //    ProvinciaNameDataList.Add(provincia.InnerText);
+            //}
+            //Picker_Provincia.ItemsSource = ProvinciaNameDataList;
         }
     }
 }
