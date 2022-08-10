20f413412cfa5a0955a89da056becaada7f58144
jhanif.kara <jhanif.kara@outlook.com>
2022-08-10 17:13:06 -0500

    video done

--- App2/App2/View/PrimeraPaginaView.xaml.cs
+++ App2/App2/View/PrimeraPaginaView.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System.IO;
 using System.Reflection;
-using System.Xml;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,16 +8,14 @@ namespace App2.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PrimeraPaginaView : ContentPage
     {
-        XmlDocument xmlinfodataloaded = new XmlDocument();
         public PrimeraPaginaView()
         {
             InitializeComponent();
             var assembly = IntrospectionExtensions.GetTypeInfo(typeof(PrimeraPaginaView)).Assembly;
-            Stream stream = assembly.GetManifestResourceStream("App2.InfomacionEnXml.xml");
-            xmlinfodataloaded.Load(stream);
-            lbl_RawXmlData.Text = xmlinfodataloaded.InnerXml;
+            Stream stream = assembly.GetManifestResourceStream("App2.CountryInfoInJson.json");
+            string StreamReader = new StreamReader(stream).ReadToEnd();
+            lbl_RawJsonData.Text = StreamReader;
+
         }
-
-
     }
 }
